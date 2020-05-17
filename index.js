ca134a628736a28189bed603f59739ebd96cd3d9
warintiras <warintiras@gmail.com>
2020-05-17 20:44:37 +0700

    Testing bot

--- index.js
+++ index.js
@@ -6,7 +6,46 @@
 // app.use('/', linebot);
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
 const app = express();
 const port = process.env.PORT || 4000;
-app.post('/linebot/webhook', (req, res) => res.sendStatus(200));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/linebot/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken;
+    reply_token(reply_token);
+    res.sendStatus(200)
+});
+
 app.listen(port);
+
+function reply(reply_token) {
+    let headers = {
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer {cyNaR/BGrcuuc/AWK9TGrZ0nHbZN2nBOIAbVmn6TsbMIVZQzm+u1swGWFr8UpKVNhF2GIhYWAKI0TQi7CjYYM//R08r2iuPiI/5nkMxWOXf7OG+WaVyIC/1tpJuwz6eemIoQryMErjz42XwGAi/KSQdB04t89/1O/w1cDnyilFU=}'
+    };
+
+    let body = JSON.stringify({
+        replyToken : reply_token,
+        messages : [{
+            type : 'text',
+            text : 'Hello'
+        },
+        {
+            type : 'text',
+            text : 'How are you?'
+        }]
+    });
+
+    request.post({
+        url : 'https://api.line.me/v2/bot/message/reply',
+        headers : headers,
+        body : body
+    }, (err, res, body) => {
+        console.log('status = '+res.statusCode);
+    });
+}
