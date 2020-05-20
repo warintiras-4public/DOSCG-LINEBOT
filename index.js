c2e99ca096239f4767db4c3f9a4127d782939ba8
warintiras <warintiras@gmail.com>
2020-05-20 11:29:21 +0700

    Add line-notify - not properly working

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
 const express = require('express');
+const request = require('request');
 const line = require('@line/bot-sdk');
 
+require('dotenv').config();
+
 const config = {
-    channelAccessToken: 'cyNaR/BGrcuuc/AWK9TGrZ0nHbZN2nBOIAbVmn6TsbMIVZQzm+u1swGWFr8UpKVNhF2GIhYWAKI0TQi7CjYYM//R08r2iuPiI/5nkMxWOXf7OG+WaVyIC/1tpJuwz6eemIoQryMErjz42XwGAi/KSQdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'bbaf3fd52698fed2d164975d96c7edc8'
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret
 };
 
 const app = express();
-// app.post('/linebot/webhook', line.middleware(config), (req, res) => {
-app.post('/webhook', line.middleware(config), (req, res) => {
-
+app.post('/linebot/webhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
-        // testing - to be deleted
-        res.sendStatus(200)
 });
 
 const client = new line.Client(config);
 
 function handleEvent(event) {
+    // Need to compare times, notification will be sent if incoming message has been received for more than 10 seconds
+    // First, get the current time
+    let currentDate = new Date();
+    let currentTime = currentDate.getTime();
+
+    // Compare times and send notification if needed
+    if ((currentTime - event.timestamp) > 10) {
+        // Send notification if the message has been received for more than 10 seconds
+        let token = 'xxxxxx'; // Need to get this from Line Notify
+        let message = 'Somebody has messaged you! Please check DOSCG!';
+        
+        request({
+            method: 'POST',
+            uri: 'https://notify-api.line.me/api/notify',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded' 
+            },
+            auth: {
+                'bearer': token
+            },
+            form: {
+                message: message
+            }
+        }, (err, httpResponse, body) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({
+                    httpResponse: httpResponse,
+                    body: body
+                });
+            }
+        });
+    }
+
+    // Send a reply
     if (event.type !== 'message' || event.message.type !== 'text') {
         return Promise.resolve(null);
     }
-console.log("Testing here!");
+
     return client.replyMessage(event.replyToken, {
         type: 'text',
         text: event.message.text
